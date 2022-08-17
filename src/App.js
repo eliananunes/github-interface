c14c65ce032130e0f5ef410a5bf1e030c4ea0cf0
eliananunes <elianadnunes@gmail.com>
2022-08-17 20:17:57 -0300

    adicinando o Layout, Profile e Repositories no App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Layout from "./components/layout/layout";
+import Profile from "./components/profile/profile";
+import Repositories from "./repositories/repositories";
+
 function App() {
   return (
     <div>
-      <h1>Iniciando</h1>
+      <Layout>
+        <Profile />
+        <Repositories />
+      </Layout>
     </div>
   );
 }
