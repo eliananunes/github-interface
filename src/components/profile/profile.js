6c8aa45cc04a89465020cc8c3754057541b88f76
eliananunes <elianadnunes@gmail.com>
2022-08-17 20:10:11 -0300

    rascunho dos componentes Layout e Profile

---
+++ src/components/profile/profile.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Profile() {
+    return(
+        <div>
+            <img
+                src="https://avatars.githubusercontent.com/u/85793463?v=4"
+                alt="user"
+            />
+            <h1>Eliana Nunes</h1>
+            <h3>Username:</h3>
+            <a
+                href="https://github.com/eliananunes"
+
+            >
+                eliananunes
+            </a>
+            <h3>Redes:</h3>
+            <a
+                href="https://www.linkedin.com/in/elianadn/"
+
+            >
+                Contato
+            </a>
+        </div>
+    );
+}
+
+export default Profile;
